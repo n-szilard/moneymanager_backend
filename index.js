2da9a53f1110f342329ffd76ad4ccb6812320e70
n-szilard <nagyapatiszilard@turr.hu>
2025-11-02 18:26:10 +0100

    Init commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const dotenv = require('dotenv').config();
+const cors = require('cors');
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended: true}));
+
+app.listen(process.env.PORT, () => console.log(`Server is running on port ${process.env.PORT}`));
